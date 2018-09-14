d1369600622e55c6ced9e957a223d7e0e8774d81
גל שחר <galsh20@gmail.com>
2018-09-14 04:07:15 +0300

    contact portfolio commit

--- dbController.js
+++ dbController.js
@@ -643,3 +643,44 @@ exports.sendContactForm = function(req, res){
 	}
 	else return res.status(500).send();
 };
+
+// send Contact Form - Shadow Yoga Shala
+exports.sendContactGal = function(req, res){
+	console.log("Start Save Contact Form...");
+	if(req.body.contactForm){
+		console.log("JSON: "+JSON.stringify(req.body.contactForm, null, 4));
+		res.status(200).send();
+		var now = new Date();
+
+
+		const nodemailer = require('nodemailer');
+		// create reusable transporter object using the default SMTP transport
+		let transporter = nodemailer.createTransport({
+		    service: 'gmail',
+		    auth: {
+		        user: 'cricketownIL@gmail.com',
+		        pass: 'cricket838495922Af3_3'
+		    }
+		});
+    	// setup email data with unicode symbols
+		let mailOptions = {
+		    from: '"Gal Portfolio Msg" <cricketownIL@gmail.com>', // sender address
+		    // to: "galsh20@gmail.com", // list of receivers
+		    to: "galsh20@gmail.com", // list of receivers
+		    subject: "Gal Portfolio - New Message", // Subject line
+		    text: "שם מלא: "+req.body.contactForm.fullName+"\n\n"+
+		    		'כתובת דוא"ל: '+req.body.contactForm.email+"\n\n"+
+		    		'תוכן ההודעה: \n'+req.body.contactForm.message		
+		};
+
+		// send mail with defined transport object
+		transporter.sendMail(mailOptions, (error, info) => {
+		    if (error) {
+		        return console.log(error);
+		    }
+		    console.log('Message %s sent: %s', info.messageId, info.response);
+		});
+
+	}
+	else return res.status(500).send();
+};
