d26a9828adfe150c9e8bbb8bd331d94eecbfd891
גל שחר <galsh20@gmail.com>
2018-09-16 02:50:11 +0300

    analytics 2

--- defineSchema/Log.js
+++ defineSchema/Log.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 var schema = mongoose.Schema;
 
 var logSchema = new schema({
-	_id: {type: String, index:1, required:true, unique:true},
+	_id: String,,
 	logUserId: String,
 	logDate: String,
 	logAction: String,
--- defineSchema/Logs.js
+++ defineSchema/Logs.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 var schema = mongoose.Schema;
 
 var logsSchema = new schema({
-	_id: {type: String, index:1, required:true, unique:true},
+	_id: String,
 	logType: String,
 	logsArr: []
 }, {collection: 'analytics'});
